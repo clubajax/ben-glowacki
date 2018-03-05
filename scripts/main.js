848440ef5849df0a82d55528de6ef7f2f578809c
Mike Wilcox <anm9tr@gmail.com>
2018-03-05 08:35:43 -0600

    grid layout

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,17 @@ const files =
 if (location.hostname === 'localhost') {
 	document.write(files);
 }
+
+const SCROLL_AMT = 160;
+let topShowing = true;
+document.querySelector('main').addEventListener('scroll', function (e) {
+	const pos = e.target.scrollTop;
+	console.log('pos', pos);
+	if (pos >= SCROLL_AMT && topShowing) {
+		topShowing = false;
+		document.body.classList.add('hide-top');
+	} else if (pos < SCROLL_AMT && !topShowing) {
+		topShowing = true;
+		document.body.classList.remove('hide-top');
+	}
+});
